72522ba4fc79f8513864e81fa0e2011ddf9202f8
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-02-02 22:04:58 +0530

    Add files via upload

--- LectureOne.py
+++ LectureOne.py
@@ -2,4 +2,12 @@ num1 = float(input('give me a number: '))
 num2 = float(input('give me second number: '))
 num3 = float(input('give me third number: '))
 num4 = float(input('give me fourth number: '))
-print( (num1 - num2) * num3 / num4 )
+print('q=arithmetic mean, w=geometric mean e=(num1 - num2) * num3 / num4')
+choice = input("Enter choice(q/w/e): ")
+import math
+if choice == 'q':
+    print((num1 + num2)/2)
+elif choice == 'w':
+    print(math.sqrt(num1 * num2) )
+elif choice == 'e':
+    print( (num1 - num2) * num3 / num4 )
