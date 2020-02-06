42032914041b3e4cc10d1747e892a159c6bb8c9c
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-02-06 22:22:46 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -11,16 +11,19 @@ display.grid(row = 0, column = 0 , columnspan = 4, pady=10, padx=4)
 def myFunction():
     value = Entry.get(display) #get display's entry
     myFun = float(value) * 0.967 # tax calc
-    display.delete(0, 1)
+    display.delete(0, END)
     display.insert(0, myFun)
-
+def cancel():
+    display.delete(0, 1)
+def cancel_all():
+    display.delete(0, END)
 
 #Search - bar 
 
 #button = btn
 btn_my = Button(ui, text='M', width = 5, command = myFunction)
-btn_c = Button(ui, text='C', width = 5)
-btn_CE = Button(ui, text='CE', width = 5)
+btn_c = Button(ui, text='C', width = 5, command = cancel)
+btn_CE = Button(ui, text='CE', width = 5, command = cancel_all)
 btn_e = Button(ui, text='=', width = 5)
 
 #button grid
@@ -28,4 +31,5 @@ btn_my.grid(row = 1, column=0, pady = 3)#computer always think that 0 = one 1 = 
 btn_c.grid(row = 1, column=1)
 btn_CE.grid(row = 1, column=2)
 btn_e.grid(row = 1, column = 3)
+
 ui.mainloop() # continuously loop
