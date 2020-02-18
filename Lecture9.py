e4eac7a90876ac7f0cff0070a1d7093287b94062
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-02-19 00:12:05 +0800

    Create Lecture9.py

---
+++ Lecture9.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+is_blue = True
+x = 30
+y = 30
+clock = pygame.time.Clock()
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_blue = not is_blue
+
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        y -= 1
+        if x < 21 or y < 21:
+            x = 350
+            y = 250
+    if pressed[pygame.K_DOWN]: 
+        y += 1
+        if x < 21 or y < 21:
+            x = 350
+            y = 250
+    if pressed[pygame.K_LEFT]: 
+        x -= 1
+        if x < 21 or y < 21:
+            x = 350
+            y = 250
+    if pressed[pygame.K_RIGHT]: 
+        x += 1
+        if x < 21 or y < 21:
+            x = 350
+            y = 250
+    screen.fill((0, 0, 0))
+    if is_blue: color = (0, 128, 255)
+    else: color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 6, 6))
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(20, 20, 10000, 1))
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(20, 20, 1, 10000))
+    pygame.display.flip()
+    clock.tick(60)
