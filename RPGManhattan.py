aa731050be172a29185674eed933d17ba0364eb2
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-03-08 23:11:39 +0800

    Update RPGManhattan.py

--- RPGManhattan.py
+++ RPGManhattan.py
@@ -4,10 +4,10 @@ from random import sample
 
 pygame.init()
 
-walkRight = [pygame.image.load('Game/R1.png'), pygame.image.load('Game/R2.png'), pygame.image.load('Game/R3.png'), pygame.image.load('Game/R4.png'), pygame.image.load('Game/R5.png'), pygame.image.load('Game/R6.png'), pygame.image.load('Game/R7.png'), pygame.image.load('Game/R8.png'), pygame.image.load('Game/R9.png')]
-walkLeft = [pygame.image.load('Game/L1.png'), pygame.image.load('Game/L2.png'), pygame.image.load('Game/L3.png'), pygame.image.load('Game/L4.png'), pygame.image.load('Game/L5.png'), pygame.image.load('Game/L6.png'), pygame.image.load('Game/L7.png'), pygame.image.load('Game/L8.png'), pygame.image.load('Game/L9.png')]
+walkRight = [pygame.image.load('MainChara/R1.png'), pygame.image.load('MainChara/R2.png'), pygame.image.load('MainChara/R3.png')]#, pygame.image.load('Game/R4.png'), pygame.image.load('Game/R5.png'), pygame.image.load('Game/R6.png'), pygame.image.load('Game/R7.png'), pygame.image.load('Game/R8.png'), pygame.image.load('Game/R9.png')]
+walkLeft = [pygame.image.load('MainChara/L1.png'), pygame.image.load('MainChara/L2.png'), pygame.image.load('MainChara/L3.png')]#, pygame.image.load('Game/L4.png'), pygame.image.load('Game/L5.png'), pygame.image.load('Game/L6.png'), pygame.image.load('Game/L7.png'), pygame.image.load('Game/L8.png'), pygame.image.load('Game/L9.png')]
 bg = pygame.image.load('Game/bg.jpg')
-char = pygame.image.load('Game/standing.png')
+char = pygame.image.load('MainChara/standing.png')
 
 #walkRight[4] = 5번째 사진??? 0번째가 첫번째다!!!
 
