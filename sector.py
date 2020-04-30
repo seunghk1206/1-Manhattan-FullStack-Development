0c49acc20cd3c2aef1c79034beb327c07fd036c8
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-04-30 16:04:05 +0900

    Update sector.py

--- sector.py
+++ sector.py
@@ -7,29 +7,45 @@ limit = int(input("what would you set as a limit? "))
 absVal = []
 EL = []
 values = []
-negativeValues = []
+#negativeValues = []
 sum = 0
 if 3 <= howMany <= 100:
     Cnumbers = list(input().split(', '))
     if len(Cnumbers) == howMany:
+        Cnumbers.sort()
         for each in Cnumbers:
             EL.append(int(each) - int(limit))
-        sorted(EL)
-        for eachNum in EL:
-            if eachNum < 0:
-                negativeValues.append(eachNum)
-            print(negativeValues)
+        # for eachNum in EL:
+        #     if eachNum < 0:
+        #         negativeValues.append(eachNum)
+        #     print(negativeValues)
         for each in EL:
             absVal.append(abs(each))
             print(absVal)
-        for each in absVal:
+        for each in range(3):
             Index = int(absVal.index(min(absVal)))
-            IndexX = int(negativeValues.index(max(negativeValues)))
-            if Index > IndexX:
-                values.append(min(absVal)+limit)
-            elif Index <= IndexX:
-                values.append(-min(absVal)+limit)
-            absVal.remove(min(absVal))
+            # IndexX = int(negativeValues.index(max(negativeValues)))
+            print(Cnumbers[Index])
+            values.append(Cnumbers[Index])
+            # if IndexX == 0:
+            #     if len(negativeValues) == 0:
+            #         values.append(min(absVal)+limit)
+            #         print(values)
+            #     elif len(negativeValues) == 1:
+            #         values.append(min(EL))
+            #     absVal.remove(min(absVal))
+            # elif IndexX > 0:
+            #     if Index > IndexX:
+            #         values.append(min(absVal)+limit)
+            #         print(values)
+            #     elif Index <= IndexX:
+            #         values.append(-min(absVal)+limit)
+            #         print(values)
+            #         negativeValues.remove(negativeValues[IndexX])
+            #         print(negativeValues)
+            for i in enumerate(absVal):
+                if i == min(absVal):
+                    absVal[Index] = 300000
         for a in range(len(values)):
             sum += int(values[a])
         print(sum)
