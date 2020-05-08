1d08b23d5e8645a8febf2c680c5a9af1c6edd344
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-05-08 12:12:15 +0900

    Update sector.py

--- sector.py
+++ sector.py
@@ -1,27 +1,23 @@
-howMany = int(input("how many cards? "))
-limit = int(input("what would you set as a limit? "))
-absVal = []
-EL = []
-values = []
-sum = 0
-if 3 <= howMany <= 100:
-    Cnumbers = list(input().split(', '))
-    if len(Cnumbers) == howMany:
-        Cnumbers.sort()
-        for each in Cnumbers:
-            EL.append(int(each) - int(limit))
-        for each in EL:
-            absVal.append(abs(each))
-        for each in range(3):
-            Index = int(absVal.index(min(absVal)))
-            print(Cnumbers[Index])
-            values.append(Cnumbers[Index])
-            Cnumbers.remove(Cnumbers[Index])
-            absVal.remove(absVal[Index])
-        for a in range(len(values)):
-            sum += int(values[a])
-        print(sum)
-    else:
-        print("only", howMany, "cards accepted")
-else:
-    print("too many cards, error")
+a= int(input("How many vocabs required? "))
+length = []
+len2 = []
+words = []
+for i in range(a):
+    inputx = input()
+    length.append(len(inputx))
+    words.append(inputx)
+for each in range(len(length)):
+    len2.append(length[each])
+print(len2)
+for i in range(len(length)):
+    for x in range(len(len2)):
+        if length[i] == len2[x]:
+            if words[len2.index(len2[x])] > words[length.index(length[i])] or words[length.index(length[i])] > words[len2.index(len2[x])]:
+                words[len2.index(len2[x])], words[length.index(length[i])] = words[len2.index(len2[x])], words[length.index(length[i])]
+            else:
+                pass
+for m in range(len(length)):
+    minLn = length.index(min(length))
+    print(words[minLn])
+    length.remove(min(length))
+    words.remove(words[minLn])
