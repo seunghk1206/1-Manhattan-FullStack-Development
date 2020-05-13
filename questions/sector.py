b39f4fc617a869e659184aeb02dc8ac971cd4b34
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-05-14 00:06:25 +0900

    Update sector.py

--- questions/sector.py
+++ questions/sector.py
@@ -1,23 +1,20 @@
-a= int(input("How many vocabs required? "))
-length = []
-len2 = []
-words = []
-for i in range(a):
-    inputx = input()
-    length.append(len(inputx))
-    words.append(inputx)
-for each in range(len(length)):
-    len2.append(length[each])
-print(len2)
-for i in range(len(length)):
-    for x in range(len(len2)):
-        if length[i] == len2[x]:
-            if words[len2.index(len2[x])] > words[length.index(length[i])] or words[length.index(length[i])] > words[len2.index(len2[x])]:
-                words[len2.index(len2[x])], words[length.index(length[i])] = words[len2.index(len2[x])], words[length.index(length[i])]
-            else:
-                pass
-for m in range(len(length)):
-    minLn = length.index(min(length))
-    print(words[minLn])
-    length.remove(min(length))
-    words.remove(words[minLn])
+NInput = int(input())
+xcoor = []
+ycoor = []
+for i in range(NInput):
+    x, y = map(int, input().split())
+    xcoor.append(x)
+    ycoor.append(y)
+for i in range(0, len(xcoor)):
+    minIndex = i
+    for j in range(len(xcoor)):
+        if xcoor[j] < xcoor[minIndex]:
+            xcoor[i], xcoor[minIndex] = xcoor[minIndex], xcoor[i]
+            ycoor[i], ycoor[minIndex] = ycoor[minIndex], ycoor[i]
+for i in range(0, len(xcoor)):
+    if i+1 < len(xcoor):
+        if xcoor[i] == xcoor[i+1]:
+            if ycoor[i] > ycoor[i+1]:
+                ycoor[i], ycoor[i+1] = ycoor[i+1], ycoor[i]
+for i in range(0, len(xcoor)):
+    print(xcoor[i], ",", ycoor[i])
