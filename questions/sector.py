6886dc2f0352cb924d317568a385d8697c7c464a
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-05-14 22:11:57 +0900

    Update sector.py

--- questions/sector.py
+++ questions/sector.py
@@ -7,14 +7,14 @@ for i in range(NInput):
     ycoor.append(y)
 for i in range(0, len(xcoor)):
     minIndex = i
-    for j in range(len(xcoor)):
+    for j in range(i+1, len(xcoor)):
         if xcoor[j] < xcoor[minIndex]:
-            xcoor[i], xcoor[minIndex] = xcoor[minIndex], xcoor[i]
-            ycoor[i], ycoor[minIndex] = ycoor[minIndex], ycoor[i]
+            xcoor[j], xcoor[minIndex] = xcoor[minIndex], xcoor[j]
+            ycoor[j], ycoor[minIndex] = ycoor[minIndex], ycoor[j]
 for i in range(0, len(xcoor)):
     if i+1 < len(xcoor):
         if xcoor[i] == xcoor[i+1]:
             if ycoor[i] > ycoor[i+1]:
                 ycoor[i], ycoor[i+1] = ycoor[i+1], ycoor[i]
 for i in range(0, len(xcoor)):
-    print(xcoor[i], ",", ycoor[i])
+    print(xcoor[i], " ", ycoor[i])
