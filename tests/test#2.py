7a93144243b64e368165fd4d18aeb570e7992971
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-05-17 19:55:18 +0900

    Update test#2.py

--- tests/test#2.py
+++ tests/test#2.py
@@ -3,27 +3,27 @@ def solution(A):
     for each in range(len(A)):
         if A[each] == '1' or A[each] == '[1' or A[each] == '1]':
             a = A.count('6') * 2
-            ln = len(A) - A.count('6') - A.count('1') - A.count('[1') - A.count('1]') - A.count('[6') - A.count('6]')
+            ln = len(A) - A.count('6') - A.count('1') - A.count('[1') - A.count('1]')
             countList.append(a + ln)
         elif A[each] == '2' or A[each] == '[2' or A[each] == '2]':
             a = A.count('5') * 2
-            ln = len(A) - A.count('5') - A.count('2') - A.count('[2') - A.count('2]') - A.count('[5') - A.count('5]')
+            ln = len(A) - A.count('5') - A.count('2') - A.count('[2') - A.count('2]')
             countList.append(a + ln)
         elif A[each] == '3' or A[each] == '[3' or A[each] == '3]':
             a = A.count('4') * 2
-            ln = len(A) - A.count('4') - A.count('3') - A.count('[3') - A.count('3]') - A.count('[4') - A.count('4]')
+            ln = len(A) - A.count('4') - A.count('3') - A.count('[3') - A.count('3]')
             countList.append(a + ln)
         elif A[each] == '4' or A[each] == '[4' or A[each] == '4]':
             a = A.count('3') * 2
-            ln = len(A) - A.count('3') - A.count('4') - A.count('[4') - A.count('4]') - A.count('[3') - A.count('3]')
+            ln = len(A) - A.count('3') - A.count('4') - A.count('[4') - A.count('4]')
             countList.append(a + ln)
         elif A[each] == '5' or A[each] == '[5' or A[each] == '5]':
             a = A.count(2) * 2
-            ln = len(A) -A.count('2') - A.count('5') - A.count('[5') - A.count('5]') - A.count('[2') - A.count('2]')
+            ln = len(A) -A.count('2') - A.count('5') - A.count('[5') - A.count('5]')
             countList.append(a + ln)
         elif A[each] == '6' or A[each] == '[6' or A[each] == '6]':
             a = A.count('1') * 2
-            ln = len(A) -A.count('1') - A.count('6') - A.count('[6') - A.count('6]') - A.count('[1') - A.count('1]')
+            ln = len(A) -A.count('1') - A.count('6') - A.count('[6') - A.count('6]')
             countList.append(a + ln)
         else:
             pass
