94fd979de2f4365f20b3096ea2d7934d75fb703a
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-05-24 01:14:29 +0900

    Update 3dList.py

--- 잡다한것들/3dList.py
+++ 잡다한것들/3dList.py
@@ -1,7 +1,6 @@
 a = int(input("which 3d list? "))
 b = [[[0, 1, 1], [0, 2, 1], [1, 4, 3]], [[0, 1, 3], [0, 0, 0], [3, 4, 2]], [[2, 3, 4], [1, 3, 2], [4, 3, 0]]]
 dummy = []
-print(b[1][0][1])
 for each in range(len(b)):
     for eachNum in range(len(b)):
         checkingList = b[a-1][int(each)][int(eachNum)]
