f6bba6fe2b62d15606078f3afd368cf8859ba9ed
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-06-19 21:53:26 +0900

    Create question#13.py

---
+++ questions/question#13.py
@@ -0,0 +1,10 @@
+import random
+a = input()
+splitl = []
+splitl.append(a.split())
+def split3(LoSp):
+    temp = []
+    for each in range(randint(1,len(LoSp))):
+        temp.append(LoSp[each])
+    
+for each in range(len(splitl)):
