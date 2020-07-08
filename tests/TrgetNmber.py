c7831c002692b91f9f0eb50cd3941e610ce373df
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-07-08 21:30:38 +0900

    Create TrgetNmber.py

---
+++ tests/TrgetNmber.py
@@ -0,0 +1,22 @@
+from itertools import product
+def solution(numbers, target):
+    answer = 0
+    tupNN = []
+    LoN = []
+    negNum = (target - sum(numbers))/2
+    for each in range(len(numbers)):
+        if each == abs(negNum):
+            answer += 1
+            numbers.remove(each)
+        else:
+            pass
+    for each in numbers:
+        tupNN.append(-each)
+    tupN = tuple(numbers)
+    result = list(product(*LoN))
+    for eachL in result:
+        if sum(eachL) == abs(negNum):
+            answer += 1
+    return answer
+solution([1, 1, 1, 1, 1], 3)
+1-1
