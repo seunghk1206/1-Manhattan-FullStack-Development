7a06f2952962197ba644cab99e3cf32d1179a4dd
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-09-28 22:04:48 +0900

    Update question#26.py

--- questions/question#26.py
+++ questions/question#26.py
@@ -1,19 +1,25 @@
 def solution(arrows):
     answer = 0
-    tempL = []
     coorL = [[0,0]]
     for each in arrows:
-        if each == 0: coorL.append([int(coorL[-1][0]), int(coorL[-1][1])+1])
-        elif each == 1: coorL.append([int(coorL[-1][0])+1, int(coorL[-1][1])+1])
-        elif each == 2: coorL.append([int(coorL[-1][0])+1, int(coorL[-1][1])])
-        elif each == 3: coorL.append([int(coorL[-1][0])+1, int(coorL[-1][1])-1])
-        elif each == 4: coorL.append([int(coorL[-1][0]), int(coorL[-1][1])-1])
-        elif each == 5: coorL.append([int(coorL[-1][0])-1, int(coorL[-1][1])-1])
-        elif each == 6: coorL.append([int(coorL[-1][0])-1, int(coorL[-1][1])])
-        elif each == 7: coorL.append([int(coorL[-1][0])-1, int(coorL[-1][1])+1])
-    for each in coorL:
-        if each in tempL:
+        if each == 0: 
+            a = [int(coorL[-1][0]), int(coorL[-1][1])+1]
+        elif each == 1: 
+            a = [int(coorL[-1][0])+1, int(coorL[-1][1])+1]
+        elif each == 2: 
+            a = [int(coorL[-1][0])+1, int(coorL[-1][1])]
+        elif each == 3: 
+            a = [int(coorL[-1][0])+1, int(coorL[-1][1])-1]
+        elif each == 4: 
+            a = [int(coorL[-1][0]), int(coorL[-1][1])-1]
+        elif each == 5: 
+            a = [int(coorL[-1][0])-1, int(coorL[-1][1])-1]
+        elif each == 6: 
+            a = [int(coorL[-1][0])-1, int(coorL[-1][1])]
+        elif each == 7: 
+            a = [int(coorL[-1][0])-1, int(coorL[-1][1])+1]
+        if a in coorL:
             answer += 1
-        tempL.append(each)
+        coorL.append(a)
     return answer
 print(solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]))
