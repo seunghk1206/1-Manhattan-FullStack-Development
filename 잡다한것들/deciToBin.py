c1f08530fd048eb4150c70e6f1a22dc6abb46a2a
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-02 12:14:53 +0900

    Create deciToBin.py

---
+++ 잡다한것들/deciToBin.py
@@ -0,0 +1,15 @@
+#import sys
+#sys.setrecursionlimit(10**4)
+def deciToBin(n, TargetNum):
+    if TargetNum >=0 :
+        if TargetNum == 0:
+            return 0
+        if 2**(n+1) > TargetNum and TargetNum >= 2**(n) and n == 0:
+            return 1
+        elif 2**(n+1) > TargetNum and TargetNum >= 2**(n):
+            return 10**n + deciToBin(0, TargetNum-(2**(n)))
+        else:
+            return deciToBin(n+1, TargetNum)
+    elif TargetNum < 0:
+        return -1*deciToBin(0, -TargetNum)
+print(deciToBin(0, float(eval(input()))))
