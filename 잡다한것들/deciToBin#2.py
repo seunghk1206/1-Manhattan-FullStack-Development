407714a19557d0da8612bd665d82925cf1eb5bd9
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-03 15:39:36 +0900

    Update deciToBin#2.py

--- 잡다한것들/deciToBin#2.py
+++ 잡다한것들/deciToBin#2.py
@@ -4,7 +4,7 @@ def deciToBin(n, TargetNum):
             return 10**(n-1) + deciToBin(0, TargetNum-(2**(n-1)))
         else:
             return deciToBin(n-1, TargetNum)
-    elif TargetNum >=0 :
+    elif TargetNum >=0:
         if TargetNum == 0:
             return 0
         if 2**(n+1) > TargetNum and TargetNum >= 2**(n):
