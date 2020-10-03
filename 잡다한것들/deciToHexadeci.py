d8120859b7f31e7dc279b40d2c319ecf4d37ee23
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-03 15:39:29 +0900

    Update deciToHexadeci.py

--- 잡다한것들/deciToHexadeci.py
+++ 잡다한것들/deciToHexadeci.py
@@ -1,26 +1,15 @@
-def deci2Hexadeci(maxn, TargetNum):
+def deci2Hexadeci(n, TargetNum):
     hexaL = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    Ans = ''
-    for each in range(maxn):
-        if 16**(abs(each-maxn)+1) > TargetNum >= 16**abs(each-maxn):
-            Ans += str(hexaL[TargetNum//(16**abs(each-maxn))])
-        else:
-            Ans += '0'
-    return Ans
+    if n > 0:
+        return str(hexaL[TargetNum//(16**n)]) + deci2Hexadeci(n-1, TargetNum%(16**n))
+    if n == 0:
+        return str(hexaL[TargetNum//(16**n)])
 def deciToHexadeci(n, TargetNum):
     hexaL = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    Ans = ''
-    run = True
-    if TargetNum != 0:
-        while run:
-            if 16**(n+1) > TargetNum >= 16**n:
-                for each in range(n):
-                    if 16**(abs(each-n)+1) > TargetNum >= 16**abs(each-n):
-                        Ans += str(hexaL[TargetNum//(16**abs(each-n))])
-                    else:
-                        Ans += '0'
-                return Ans
-            n+=1
+    if 16**(n+1) > TargetNum >= 16**n:
+        return str(hexaL[TargetNum//(16**n)]) + str(deci2Hexadeci(n-1, TargetNum%(16**n)))
     elif TargetNum == 0:
         return 0
-print(deciToHexadeci(0, 9))
+    else: 
+        return deciToHexadeci(n+1, TargetNum)
+print(deciToHexadeci(0, 200))
