f805373f1e9ed40d877c73637abe6f6976111c12
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-04 18:20:29 +0900

    Update percent.py

--- 잡다한것들/percent.py
+++ 잡다한것들/percent.py
@@ -1,10 +1,14 @@
-def percent(var):
-    b = str(var)
+def Slalash(var):
+    print(var)
+    b = str(eval(var))
     if '.' in b:
         a = list(b.split('.'))
         return a[0]
     else:
         return b
-
-print(percent(50/23))
+print(str(input()))
+print(Slalash(str(50/23)))
 print(50//23)
+def percent(var):
+    return int(var.split('/')[0]) - (int(Slalash(var)) * int(var.split('/')[1]))
+print(percent(str(input())))
