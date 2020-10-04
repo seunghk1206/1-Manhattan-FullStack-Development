6ce1961b4b10cbf61130f61ba059206d2e54ff62
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-04 15:00:09 +0900

    Update Chain.py

--- 잡다한것들/Chain.py
+++ 잡다한것들/Chain.py
@@ -1,9 +1,15 @@
-def iterChain(List):
-    L = []
+import sys
+sys.setrecursionlimit(10**6)
+L=[]
+temL = []
+def iterChain(L, List, tempL):
     for each in range(len(List)):
         if str(type(List[each])) == "<class 'list'>":
-            return iterChain(List[each])
+            tempL.append(List[each])
         else:
             L.append(List[each])
+    if len(tempL) != 0:
+        List = []
+        return iterChain(L, tempL, List)
     return L
-print(iterChain([[[1,2,3,4,5,6,7,8,9,3,1,23,3,1,21,3,1,23,1,32,122,23,1,321,23]]]))
+print(iterChain(L, [1,[2,[1,2,3,4,5,6,7,8,9,3,1,23,3,1,21,3,1,23,1,32,122,23,1,321,23]]], temL))
