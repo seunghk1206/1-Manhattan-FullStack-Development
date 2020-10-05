0128b9ad354fe50c243fb871bd4ae8f7180b7df9
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-05 12:41:48 +0900

    Update quadToDeci.py

--- 잡다한것들/역진수/quadToDeci.py
+++ 잡다한것들/역진수/quadToDeci.py
@@ -1,11 +1,14 @@
 def quadToDeci(num):
-    ans = 0
-    a = str(num).split('.')[0][::-1]
-    for each in range(len(a)):
-        ans += int(a[each]) * 4**each
-    if '.' in str(num):
-        b = str(num).split('.')[1]
-        for each in range(1, len(b)+1):
-            ans += int(b[each-1]) * 4 ** (-1 * each)
-    return ans
+    if num >= 0:
+        ans = 0
+        a = str(num).split('.')[0][::-1]
+        for each in range(len(a)):
+            ans += int(a[each]) * 4**each
+        if '.' in str(num):
+            b = str(num).split('.')[1]
+            for each in range(1, len(b)+1):
+                ans += int(b[each-1]) * 4 ** (-1 * each)
+        return ans
+    elif num < 0:
+        return -1 * quadToDeci(-num)
 print(quadToDeci(64.2342))
