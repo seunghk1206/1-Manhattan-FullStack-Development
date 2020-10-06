d784cd4fba931c18b2fe56c572ae84beabac1ddb
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-10-06 21:35:31 +0900

    Update Chain.py

--- 잡다한것들/Chain.py
+++ 잡다한것들/Chain.py
@@ -2,14 +2,15 @@ import sys
 sys.setrecursionlimit(10**6)
 L=[]
 temL = []
-def iterChain(L, List, tempL):
+def iterChain(List):
+    tempL = []
+    L = []
     for each in range(len(List)):
         if str(type(List[each])) == "<class 'list'>":
             tempL.append(List[each])
         else:
             L.append(List[each])
     if len(tempL) != 0:
-        List = []
-        return iterChain(L, tempL, List)
+        L.append(iterChain(tempL))
     return L
-print(iterChain(L, [1,[2,[1,2,3,4,5,6,7,8,9,3,1,23,3,1,21,3,1,23,1,32,122,23,1,321,23]]], temL))
+print(iterChain([1,[2,[1,2,3,4,5,6,7,8,9,3,1,23,3,1,21,3,1,23,1,32,122,23,1,321,23]]]))
