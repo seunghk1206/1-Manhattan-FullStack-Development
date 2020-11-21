3a4a1a9c0be54bdcc2bec2866a69e278655538e2
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-11-21 14:55:06 +0900

    Create LectureOnePractice.py

---
+++ calculator/Lecture/LectureOnePractice.py
@@ -0,0 +1,10 @@
+a = input('give me work ')
+num1 = float(input('give me a number '))
+num2 = float(input('give me a second number'))
+if a == "plus":
+    print(num1 + num2)
+print(num1*num2)
+
+import math
+
+math.sqrt()#읽는 법 알려주기 책, 도서관이 좋은 예시
