f366a8de1d4f645d57400f23703ef8af1b381d62
Noble-1206 <seunghk1206@gmail.com>
2021-05-12 11:40:40 +0900

    Update MatrixClass.py

--- MatrixClass.py
+++ MatrixClass.py
@@ -2,14 +2,15 @@ class matrix:
     def __init__(self, matrix):
         self.matrix = matrix
     def TransposeMatrix(self):
-        TransposeMatrix = []
-        tempL = []
-        for i in range(len(self.matrix[0])):
-            for j in range(len(self.matrix)):
-                tempL.append(self.matrix[j][i])
-            TransposeMatrix.append(tempL)
+        if len(self.matrix) == len(self.matrix[0]):
+            TransposeMatrix = []
             tempL = []
-        return TransposeMatrix
+            for i in range(len(self.matrix[0])):
+                for j in range(len(self.matrix)):
+                    tempL.append(self.matrix[j][i])
+                TransposeMatrix.append(tempL)
+                tempL = []
+            return TransposeMatrix
     def AddMatrix(self, additionalMatrix):
         tempL = []
         retL = []
@@ -20,6 +21,13 @@ class matrix:
                 retL.append(tempL)
                 tempL = []
             return retL
+    def Diagonals(self):
+        tempL = []
+        temp2L = []
+        for i in range(len(self.matrix)):
+            tempL.append(self.matrix[i][i])
+            temp2L.append(self.matrix[i][len(self.matrix)-i])
+        return [tempL, temp2L]
     def MultiplyMatrix(self, multiplicationMatrix):
         var = 0
         tempL = []
@@ -40,3 +48,5 @@ matrix1 = matrix([[1,2,3], [1,2,0]])
 matrix2 = [[0, 5], [1, 2], [3, 4]]
 print(matrix1.MultiplyMatrix(matrix2))
 print(matrix1.TransposeMatrix())
+matrix1 = matrix([[1,2,3], [1,2,0], [1,3,2]])
+print(matrix1.Diagonals())
