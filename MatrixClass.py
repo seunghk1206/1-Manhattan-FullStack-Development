5b61923e2c5e0582ee8e40a675a63d96eccaf440
Noble-1206 <seunghk1206@gmail.com>
2021-05-18 16:18:57 +0900

    Update MatrixClass.py

--- MatrixClass.py
+++ MatrixClass.py
@@ -47,17 +47,14 @@ class matrix:
             return retL
 
 def MagicIndex(a, dim):
-    return [a//dim, a%dim-1]
+    return [a//dim, a%dim]
 
-
-# 5->2
-# 7->3
 def MagicSquare(dimension):
     if dimension%2 == 1:
         initInd = MagicIndex(dimension**2-dimension//2, dimension)
         initL = [[0 for _ in range(dimension)] for _ in range(dimension)]
-        initL[initInd[0]][initInd[1]] = 1
-        newInd = initInd
+        initL[initInd[0]][initInd[1]-1] = 1
+        newInd = [initInd[0], initInd[1]-1]
         for i in range(dimension**2-1):
             if newInd[0]+1 >= dimension:
                 rightDownInd = [(newInd[0]+1)%dimension, (newInd[1]+1)%dimension]
@@ -73,7 +70,54 @@ def MagicSquare(dimension):
                     newInd = [newInd[0]-1, newInd[1]]
                 initL[newInd[0]][newInd[1]] = i+2
         return initL
-print(MagicSquare(5))
+    # 4x4
+    # 0 -> 3 -> 5 -> 6 -> 
+    # 8x8
+    # 0 -> 3 -> 4 -> 7 -> 9
+    elif dimension%2 == 0:
+        initL = [[0 for _ in range(dimension)] for _ in range(dimension)]
+        DiagonalInd = []
+        for each in range(dimension*dimension//4):
+            if (each//dimension)%2 == 0:
+                if each%2 == 0:
+                    DiagonalInd.append(each*2)
+                else:
+                    DiagonalInd.append(each*2 + 1)
+            else:
+                if each%2 == 0:
+                    DiagonalInd.append(each*2 + 1)
+                else:
+                    DiagonalInd.append(each*2)
+        for i in range(len(DiagonalInd)):
+            DiagonalInd[i] = MagicIndex(DiagonalInd[i], dimension)
+        for each in DiagonalInd:
+            initL[each[0]][each[1]] = each[0]*dimension+each[1]+1
+        unknownNumbers = []
+        for each in range(1,dimension**2+1):
+            if each not in initL[(each-1)//dimension]:
+                unknownNumbers.append(each)
+            else:
+                pass
+        tempInd = 1
+        for i in range(len(initL)):
+            for j in range(len(initL[i])):
+                if initL[i][j] == 0:
+                    initL[i][j] = unknownNumbers[-tempInd]
+                    tempInd += 1
+        return initL
+
+def magicSquareCheck(magicSquare):
+    initSquare = matrix(magicSquare)
+    tempL = []
+    Diagonals = initSquare.Diagonals()
+    for each in magicSquare:
+        tempL.append(sum(each))
+    for each in Diagonals: 
+        tempL.append(sum(each))
+    if len(set(tempL)) == 1:
+        return True
+    return False
+print(magicSquareCheck(MagicSquare(7)))
 matrix1 = matrix([[1,2,3], [1,2,0]])
 matrix2 = [[0, 5], [1, 2], [3, 4]]
 print(matrix1.MultiplyMatrix(matrix2))
