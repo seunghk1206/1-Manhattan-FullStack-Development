0a3c02c0d8fd92ec3577e1041fb08a263c01c3ba
Noble-1206 <seunghk1206@gmail.com>
2021-05-18 22:33:03 +0900

    Create Trig.py

---
+++ Trig.py
@@ -0,0 +1,69 @@
+import sys
+
+sys.setrecursionlimit(20000)
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else: 
+        return x * factorial(x-1)
+
+def f(x, n):
+    return (((-1)**n)*x**(2*n+1)/factorial(2*n+1))
+
+def sin(x, n):
+    x = x%360
+    if x%90 == 0:
+        if (x//90)%2 == 1:
+            if ((x//90)//2)%2 == 1:
+                return -1
+            else: 
+                return 1
+        else:
+            return 0
+    else:
+        pi2deg = 3.1415/180
+        if n == 0:
+            return f(x*pi2deg, n)
+        else:
+            return f(x*pi2deg, n) + sin(x,n-1)
+
+def g(x, n):
+    return (((-1)**n)*x**(2*n)/factorial(2*n))
+
+def cos(x, n):
+    x = x%360
+    if x%90 == 0:
+        if (x//90)%2 == 1:
+            return 0
+        else:
+            if x%360 == 0:
+                return 1
+            else:
+                return -1
+    else:
+        pi2deg = 3.1415/180
+        if n == 0:
+            return g(x*pi2deg, n)
+        else:
+            return g(x*pi2deg, n) + cos(x,n-1)
+
+def tan(x, n):
+    return sin(x, n)/cos(x, n)#maximum limit n = 84
+
+def compoundAddCos(a, b):
+    return cos(a, 84)*cos(b, 84) - sin(a, 84)*sin(b, 84)
+def MultipleSin(x, limit):
+    return 2**(limit-1)*sin(x, 84)*MultipleSinBody(x, 1, limit)
+def MultipleSinBody(x, n, limit):
+    if n + 1 != limit-1:
+        return sin(n*180/limit+x, 84)*MultipleSinBody(x, n+1, limit)
+    else:
+        return sin(n*180/limit+x, 84)
+def eulerNumber(precision):
+    if precision != 0:
+        return 1/factorial(precision)+eulerNumber(precision-1)
+    else:
+        return 1
+print(MultipleSin(1, 30))
+print(eulerNumber(20))#20 is the limit threshold. It makes no difference in limit is larger than 20
