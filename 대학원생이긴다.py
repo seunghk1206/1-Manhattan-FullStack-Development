0e8472a3a7a4a78c9ff18f9ced185e570bca82d1
Seunghyeon Kim <60473607+seunghk1206@users.noreply.github.com>
2021-06-23 00:02:05 +0900

    Update 대학원생이긴다.py

--- 대학원생이긴다.py
+++ 대학원생이긴다.py
@@ -21,32 +21,41 @@ for _ in range(inp):
         for each in coorL:
             coorB.append([each[0]-dxdy[0], each[1]-dxdy[1]])
         for each in coorB:
-            if each[0] == 0 and each[1] != 0:
+            if -0.000001 <= each[0] <= 0.000001 and (0.000001 < each[1] or -0.000001 > each[1]):
                 if each[1] < 0:
                     angL.append(-90)
                 elif each[1] > 0:
                     angL.append(90)
-            elif each[0] == 0 and each[1] == 0:
+            elif -0.000001 <= each[0] <= 0.000001 and -0.000001 <= each[1] <= 0.000001:
                 pass
             else:
                 if slopeInAng(each[1]/each[0]) < 0:
                     angL.append(360 + slopeInAng(each[1]/each[0]))
                 else:
                     angL.append(slopeInAng(each[1]/each[0]))
-        absAng = [abs(each) for each in angL]
-        Rotation = angL[absAng.index(min(absAng))]
-        if Rotation < 0:
-            Rotation = 360 + Rotation
+        Rotation = angL[angL.index(min(angL))]
         factsL.append([scale, Rotation, dxdy[0], dxdy[1]])
     heatTreat = []
     angL = [each[1] for each in factsL]
     for each in range(len(angL)):
-        if int(angL[each]) == angL[each]:
-            heatTreat.append(factsL[each])
-    if angL[0] == angL[1]:
-        distL = [each[2]**2+each[3]**2 for each in heatTreat]
-        printL.append(heatTreat[distL.index(min(distL))])
-    else:
-        printL.append(heatTreat[0])
+        if int(angL[each])-0.000001 <= angL[each] <= int(angL[each]) + 0.000001:
+            if int(factsL[each][3])-0.000001 <= factsL[each][3] <= int(factsL[each][3])+0.000001 and int(factsL[each][2])-0.000001 < factsL[each][2] < int(factsL[each][2])+0.000001:
+                heatTreat.append(factsL[each])
+    distL = [each[2]**2+each[3]**2 for each in heatTreat]
+    printL.append(heatTreat[distL.index(min(distL))])
 for each in printL:
     print(each[0], each[1], each[2], each[3])
+
+'''
+3
+2 0 1 -2 3 -2
+8 -10 16 -6 8 -2
+-4 4 -2 5 -2 3
+
+5
+-8 -9 -6.585786438 -7.5857864 -8.707106781 -6.878679656
+3 3 1 7 -1 3
+5 -7 9 1 13 -7
+12 -2 8 6 4 -2
+5 6 3 5 3 7
+'''
